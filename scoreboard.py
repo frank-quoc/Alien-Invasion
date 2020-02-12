731d84103e8e5c84e2a1714e391a39e07eb937df
Oka6e <frankquocho214@gmail.com>
2020-02-11 16:34:33 -0800

    Updated Commit

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,8 @@ class Scoreboard():
 
     def prep_score(self):
         """Turn the score into a rendered image."""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color,
             self.ai_settings.bg_color)
 
